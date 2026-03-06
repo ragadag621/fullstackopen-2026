2f9f7beb2145135799c6141a0c79503e70001775
ragadag621 <ragadag62@gmail.com>
2026-03-06 07:33:07 +0200

    complete exercises 2.1-2.5

---
+++ Part3/CourseInfo/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = ({ course }) => {
+  console.log("header yes got this",course)
+
+  return (
+    <>
+      <h3>{course}</h3>
+    </>
+  )
+}
+export default Header
---
+++ Part3/CourseInfo/src/Components/Part.jsx
@@ -0,0 +1,12 @@
+const Part = ({ part }) => {
+  
+ 
+  return (
+    <>
+      <li>
+        {part.name}:{part.exercises}
+      </li>
+    </>
+  )
+}
+export default Part
