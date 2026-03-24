948d8d1b85dfee201ffdc8ec9b7422e312d2aa41
ragadag621 <ragadag62@gmail.com>
2026-03-25 01:01:17 +0200

    feat: configure  manifest

--- Part2/CountriesData/vite.config.js
+++ Part2/CountriesData/vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-refresh' // أو @vitejs/plugin-react حسب شو عندك
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Countries Explorer Pro',
+        short_name: 'CountriesApp',
+        description: 'Explore world countries, weather and currency rates',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  base: './' 
 })
